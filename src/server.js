75ce42c7e49ceba4f26c43db4c59113ee70ea9e5
terriblecodebutwork <metanet@yeah.net>
2019-10-09 17:29:03 +0800

    jsonbase get and put

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+
+const api = 'https://jsonbase.com/'
+
+let Bucket = class {
+    constructor(name) {
+        this.name = name
+    }
+
+    async get(key) {
+        let data = await fetch(api + this.name + '/' + key)
+            .then(x => x.json())
+        return data
+    }
+
+    async put(key, value) {
+        let data = await fetch(api + this.name + '/' + key, {
+            method: "PUT",
+            body: JSON.stringify(value),
+            headers: { 'content-type': 'application/json' }
+        }).then(x => x.json())
+        return data
+    }
+}
+
+let demo_bucket = new Bucket('demo_bucket');
+demo_bucket.get('joe')
+    .then(data => console.log(data))
+
+demo_bucket.put('mike', {hello: 'mike'})
+    .then(data => console.log(data))
